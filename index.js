86c08eca2b5b5ec31600e8a5790a57f5de4574f9
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-16 10:53:02 +0100

    Add database conenction and API route

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ require('dotenv').config();
 
 const app = express();
 
-const db = mysql.createConnection({});
+const db = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+db.connect();
+
+app.get('/people', (req, res) => {
+  const sql = 'SELECT * FROM people';
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
 
 app.listen(3000, () => console.log('its working, its working!'));
